371fb7817758104799067192a401ab5b274ea5a2
Manasvinitroshan <manas.comet.25@gmail.com>
2024-09-08 16:51:27 -0500

    project done

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import os
 from dotenv import load_dotenv
+import os
 
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_astradb import AstraDBVectorStore
-from langchains.agents import create_tool_calling_agent
-from langchains.agents import AgentExector
-from langchains.tools.retriever import hub
+from langchain.agents import create_tool_calling_agent
+from langchain.agents import AgentExecutor
+from langchain.tools.retriever import create_retriever_tool
+from langchain import hub
 from github import fetch_github_issues
+from note import note_tool
 
 load_dotenv()
+
 
 def connect_to_vstore():
     embeddings = OpenAIEmbeddings()
@@ -18,30 +21,56 @@ def connect_to_vstore():
 
     if desired_namespace:
         ASTRA_DB_KEYSPACE = desired_namespace
-
     else:
         ASTRA_DB_KEYSPACE = None
-    
+
     vstore = AstraDBVectorStore(
-
-        embeddings=embeddings,
-        collection_name = "github",
-        api_endpoint = ASTRA_DB_API_ENDPOINT,
-        token = ASTRA_DB_APPLICATION_TOKEN,
-        namespace = ASTRA_DB_KEYSPACE,
-
+        embedding=embeddings,
+        collection_name="github",
+        api_endpoint=ASTRA_DB_API_ENDPOINT,
+        token=ASTRA_DB_APPLICATION_TOKEN,
+        namespace=ASTRA_DB_KEYSPACE,
     )
     return vstore
 
+
 vstore = connect_to_vstore()
-add_to_vectorstore = input("Do you want to update the issues (y/N): ").lower() in ["yes", "y"]
+add_to_vectorstore = input("Do you want to update the issues? (y/N): ").lower() in [
+    "yes",
+    "y",
+]
 
 if add_to_vectorstore:
     owner = "Manasvinitroshan"
     repo = "tta-web"
-    fetch_github_issues(owner,repo)
+    issues = fetch_github_issues(owner, repo)
 
+    try:
+        vstore.delete_collection()
+    except:
+        pass
 
+    vstore = connect_to_vstore()
+    vstore.add_documents(issues)
 
+    # results = vstore.similarity_search("flash messages", k=3)
+    # for res in results:
+    #     print(f"* {res.page_content} {res.metadata}")
 
+retriever = vstore.as_retriever(search_kwargs={"k": 3})
+retriever_tool = create_retriever_tool(
+    retriever,
+    "github_search",
+    "Search for information about github issues. For any questions about github issues, you must use this tool!",
+)
 
+prompt = hub.pull("hwchase17/openai-functions-agent")
+llm = ChatOpenAI()
+
+tools = [retriever_tool, note_tool]
+agent = create_tool_calling_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+while (question := input("Ask a question about github issues (q to quit): ")) != "q":
+    result = agent_executor.invoke({"input": question})
+    print(result["output"])
